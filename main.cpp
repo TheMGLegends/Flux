6e381caccc6fa45760cdc40c6e37ded909a62616
TheMGLegends <matejovo34@gmail.com>
2025-02-15 12:44:10 +0000

    Created Project and Integrated SDL

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+int main()
+{
+	// INFO: SDL Integration Test
+	/*SDL_InitFlags flags = SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC |
+		SDL_INIT_HAPTIC | SDL_INIT_GAMEPAD | SDL_INIT_EVENTS | SDL_INIT_SENSOR | SDL_INIT_CAMERA;
+	if (SDL_Init(flags))
+	{
+		std::cout << "Initialized SDL successfully!" << std::endl;
+	}
+
+	SDL_Quit();*/
+
+	return 0;
+}
