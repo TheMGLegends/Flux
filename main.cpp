39cf1d31e43390edc8a78cc7b8bc55521c914f8b
TheMGLegends <matejovo34@gmail.com>
2025-02-15 16:50:53 +0000

    Integrated nlohmann json

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+
 #include <SDL3/SDL.h>
 #include <Mouse.h>
+#include <nlohmann/json.hpp>
 
 int main()
 {
@@ -15,6 +18,24 @@ int main()
 
 	// INFO: DirectXTK Integration Test
 	//DirectX::Mouse mouse;
+
+	// INFO: JSON Integration Test
+	/*nlohmann::ordered_json j = {
+		{"pi", 3.141},
+		{"happy", true},
+		{"name", "Niels"},
+		{"nothing", nullptr},
+		{"answer", {
+			{"everything", 42}
+		}},
+		{"list", {1, 0, 2}},
+		{"object", {
+			{"currency", "USD"},
+			{"value", 42.99}
+		}}
+	};
+
+	std::cout << j.dump(4) << std::endl;*/
 
 	return 0;
 }
