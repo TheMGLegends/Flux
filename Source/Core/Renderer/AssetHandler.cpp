bb71f4637920f867d3961b2bd50da4d2152cc69a
TheMGLegends <matejovo34@gmail.com>
2025-03-05 13:41:20 +0000

    Yeppers

--- Source/Core/Renderer/AssetHandler.cpp
+++ Source/Core/Renderer/AssetHandler.cpp
@@ -14,3 +14,12 @@ HRESULT AssetHandler::Initialise(ID3D11Device& _device, ID3D11DeviceContext& _de
 
 	return S_OK;
 }
+
+HRESULT Flux::AssetHandler::LoadAssets(const std::filesystem::path& assetDirectory)
+{
+	// TODO: Load Required Assets (Shaders, Culling Modes, Depth Writes etc.)
+
+	// TODO: Load Assets from the specified directory (Textures, Models etc.)
+
+	return S_OK;
+}
--- Source/Core/Renderer/AssetHandler.h
+++ Source/Core/Renderer/AssetHandler.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <d3d11.h>
+#include <filesystem>
 #include <optional>
+#include <SpriteFont.h>
 #include <unordered_map>
 #include <Windows.h>
+
+#include "ConstantBufferData.h"
+#include "ShaderData.h"
 
 namespace Flux
 {
@@ -17,11 +22,15 @@ namespace Flux
 
 		static HRESULT Initialise(ID3D11Device& _device, ID3D11DeviceContext& _deviceContext);
 
+		/// @brief Loads core assets as well as assets from the specified directory
+		static HRESULT LoadAssets(const std::filesystem::path& assetDirectory);
+
 	private:
 		static std::optional<std::reference_wrapper<ID3D11Device>> device;
 		static std::optional<std::reference_wrapper<ID3D11DeviceContext>> deviceContext;
 
-
+		// TODO: Maybe key should be something else like filesystem::path or part of it
+		static std::unordered_map<std::string, ShaderData> shaders;
 	};
 }
 
