605b928abb6f0bda4394132cf089efc579ab1848
TheMGLegends <matejovo34@gmail.com>
2025-08-11 21:47:31 +0100

    Further progress on project premake + added vendor library github downloads
    Will add individual premakes to the vendor libraries (Only inside the Flux directory for now) and link them to the main project premake.

---
+++ Flux/vendor/entt/src/entt.cpp
@@ -0,0 +1,3 @@
+#include "../single_include/entt/entt.hpp"
+
+// Dummy Source File so that the entt library can be compiled as a static library.
--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+-- Implementation Supports: Windows
+
 -- Useful commands to integrate:
 -- cppdialect
 -- defines
@@ -18,15 +20,112 @@
 -- targetdir
 -- warnings
 -- workspace
+-- pchheader
+-- pchsource
+-- systemversion
+-- postbuildcommands
+-- runtime
 
 workspace "Flux"
     architecture "x86_64" -- Only 64-Bit Systems are Supported
     configurations { "Debug", "Release" }
     builddir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-    -- TODO Include External Library Premakes
+    -- Startup Project
+    filter "action:vs*"
+        startproject "Sandbox"
 
+    -- TODO Include External Library Premakes (They reference externalprojects with functions for setup into main project)
+
+    -- Main Project
     project "Flux"
         location "Flux"
         kind "SharedLib"
         language "C++"
+
+        -- Build Directories
+        targetdir ("build/" .. builddir .. "/%{prj.name}")
+        objdir ("build-int/" .. builddir .. "/%{prj.name}")
+
+        -- Precompiled Header
+        pchheader "src/pch.h"
+        pchsource "src/pch.cpp"
+
+        -- Project Source Files
+        files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+
+        -- TODO Run External Library Premake Functions that setup includedirs, libdirs, links, etc.
+
+        -- Flux Engine Include Directory
+        includedirs { "%{prj.name}/src" }
+
+        -- Post Build Commands (Copy DLL into Running Project Build Directory)
+        local sourceDir = "%{cfg.buildtarget.relpath}"
+        local destinationDir = "../build/" .. builddir .. "/Sandbox"
+        postbuildcommands { "{COPYDIR} " .. sourceDir .. " " .. destinationDir }
+
+        -- Windows Specific Settings
+        filter "system:windows"
+            cppdialect "C++20"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines { "FLUX_PLATFORM_WINDOWS", "FLUX_BUILD_DLL" }
+
+        -- Debug Configuration Settings
+        filter "configurations:Debug"
+            defines { "FLUX_DEBUG" }
+            symbols "On"
+            runtime "Debug"
+
+        -- Release Configuration Settings
+        filter "configurations:Release"
+            defines { "FLUX_RELEASE" }
+            optimize "On"
+            runtime "Release"
+    
+    -- Sandbox Project
+    project "Sandbox"
+        location "Sandbox"
+        kind "ConsoleApp"
+        language "C++"
+
+        -- Build Directories
+        targetdir ("build/" .. builddir .. "/%{prj.name}")
+        objdir ("build-int/" .. builddir .. "/%{prj.name}")
+
+        -- Project Source Files
+        files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+
+        -- TODO Run External Library Premake Functions that setup includedirs, libdirs, links, etc.
+
+        -- Sandbox Include Directory
+        includedirs { "%{prj.name}/src" }
+
+        -- Link to Flux Engine
+        links { "Flux" }
+
+        -- Post Build Commands (Copy Executable into Running Project Build Directory)
+        local sourceDir = "%{cfg.buildtarget.relpath}"
+        local destinationDir = "../build/" .. builddir .. "/Sandbox"
+        postbuildcommands { "{COPYDIR} " .. sourceDir .. " " .. destinationDir }
+
+        -- Windows Specific Settings
+        filter "system:windows"
+            cppdialect "C++20"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines { "FLUX_PLATFORM_WINDOWS" }
+
+        -- Debug Configuration Settings
+        filter "configurations:Debug"
+            defines { "FLUX_DEBUG" }
+            symbols "On"
+            runtime "Debug"
+
+        -- Release Configuration Settings
+        filter "configurations:Release"
+            defines { "FLUX_RELEASE" }
+            optimize "On"
+            runtime "Release"
