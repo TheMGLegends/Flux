b7b539395d818b5193b50e4a48b9614d1d757de4
TheMGLegends <matejovo34@gmail.com>
2025-08-12 21:57:33 +0100

    Converted library compilation to happen during project building

--- Flux/vendor/spdlog/premake5.lua
+++ Flux/vendor/spdlog/premake5.lua
@@ -1,27 +1,33 @@
--- Build using CMakeLists for Windows (VS 2022)
-
-if os.host() == "windows" then
-    -- Debug Build
-    os.execute('cmake -S . -B build/debug -G "Visual Studio 17 2022" -A x64 -DSPDLOG_USE_STD_FORMAT=ON -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug -DCMAKE_INSTALL_PREFIX=install/debug')
-    os.execute('cmake --build build/debug --config Debug')
-    os.execute('cmake --install build/debug --config Debug --prefix install/debug')
-    
-    -- Release Build
-    os.execute('cmake -S . -B build/release -G "Visual Studio 17 2022" -A x64 -DSPDLOG_USE_STD_FORMAT=ON -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX=install/release')
-    os.execute('cmake --build build/release --config Release')
-    os.execute('cmake --install build/release --config Release --prefix install/release')
-end
+---@diagnostic disable: undefined-global, undefined-field
 
 function UseSPDLOG()
+    local sourceDir = "vendor/spdlog"
+
     defines { "SPDLOG_COMPILED_LIB", "SPDLOG_USE_STD_FORMAT" }
     includedirs { "Flux/vendor/spdlog/include" }
 
     filter "configurations:Debug"
+        -- Debug Build for Windows (VS 2022)
+        prebuildcommands
+        {
+            'cmake -S ' .. sourceDir .. ' -B ' .. sourceDir .. '/build/debug -G "Visual Studio 17 2022" -A x64 -DSPDLOG_USE_STD_FORMAT=ON -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDebug -DCMAKE_INSTALL_PREFIX=' .. sourceDir .. '/install/debug',
+            'cmake --build ' .. sourceDir .. '/build/debug --config Debug',
+            'cmake --install ' .. sourceDir .. '/build/debug --config Debug --prefix ' .. sourceDir .. '/install/debug'
+        }
+
         links { "spdlogd" }
         libdirs { "Flux/vendor/spdlog/install/debug/lib" }
     filter {}
     
     filter "configurations:Release"
+        -- Release Build for Windows (VS 2022)
+        prebuildcommands
+        {
+            'cmake -S ' .. sourceDir .. ' -B ' .. sourceDir .. '/build/release -G "Visual Studio 17 2022" -A x64 -DSPDLOG_USE_STD_FORMAT=ON -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX=' .. sourceDir .. '/install/release',
+            'cmake --build ' .. sourceDir .. '/build/release --config Release',
+            'cmake --install ' .. sourceDir .. '/build/release --config Release --prefix ' .. sourceDir .. '/install/release'
+        }
+
         links { "spdlog" }
         libdirs { "Flux/vendor/spdlog/install/release/lib" }
     filter {}
